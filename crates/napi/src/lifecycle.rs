6474a85992f26ff9231c93b2aac74b7333b0c754
HotQ <rtge@outlook.com>
2023-03-23 20:22:57 +0800

    wup

--- crates/napi/src/lifecycle.rs
+++ crates/napi/src/lifecycle.rs
@@ -1,16 +1,21 @@
+use napi_sys::{napi_env, napi_env__};
+
 use crate::env::Env;
 use std::ffi::c_void;
 use std::ptr::null_mut;
 use std::ptr::NonNull;
 use std::sync::atomic::AtomicPtr;
 use std::sync::atomic::Ordering::SeqCst;
+use std::sync::Arc;
+use std::sync::RwLock;
 
 pub struct Lifecycle;
 
 type AtomicEnv = AtomicPtr<napi_sys::napi_env__>;
 
 thread_local! {
-    static CRT_RAW_ENV: AtomicEnv = AtomicPtr::new(null_mut());
+    static CRT_RAW_ENV0: AtomicEnv = AtomicPtr::new(null_mut());
+    static CRT_RAW_ENV: Arc<AtomicEnv> = Arc::new(AtomicPtr::new(null_mut()));
 }
 
 impl Lifecycle {
@@ -30,7 +35,7 @@ impl Lifecycle {
   }
 
   pub fn is_valid(env: Env) -> bool {
-    CRT_RAW_ENV.with(|env_ptr| env_ptr.load(SeqCst) == env.raw())
+    CRT_RAW_ENV.with(|env_ptr| std::ptr::eq(env_ptr.load(SeqCst), env.raw()))
   }
 
   pub fn get_env(env: Env) -> Option<Env> {
@@ -46,3 +51,13 @@ unsafe extern "C" fn unregister(env_ptr: *mut c_void) {
     napi_sys::napi_remove_env_cleanup_hook(raw_env, Some(unregister), env_ptr);
   }
 }
+
+pub struct Bound<T> {
+    pub(crate) inner: T,
+    pub(crate) raw_env: *mut napi_env,
+  }
+
+  pub struct AtomicBound<T> {
+    pub(crate) inner: T,
+    pub(crate) raw_env: *mut napi_env,
+  }
